8247df905577a4e430dd61060117d0495292bba2
Matt <matthieutc@yahoo.com>
2011-03-06 15:18:34 -0600

    added comments to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,19 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
+# random text taken from wikipedia
 @@text = %{The IMF's influence in the global economy steadily increased as it accumulated more members. The number of IMF member countries has more than quadrupled from the 44 states involved in its establishment, reflecting in particular the attainment of political independence by many developing countries and more recently the collapse of the Soviet bloc. The expansion of the IMF's membership, together with the changes in the world economy, have required the IMF to adapt in a variety of ways to continue serving its purposes effectively.}
+
+def random_text
+  words = @@text.split
+  first = rand(words.size - 15)
+  last = rand(15)
+  words[first, last].join " "
+end
+
+def random_user
+  User.find(rand(User.all.size) + 1)
+end
 
 def create_users
   users = ["john", "mike", "paul", "betty", "ashley"].map do |username|
@@ -16,21 +28,24 @@ def create_users
   User.create(users)
 end
 
-def random_text
-  words = @@text.split
-  first = rand(words.size - 15)
-  last = rand(15)
-  words[first, last].join " "
+def create_submissions
+  30.times do
+    Submission.create([{:url => random_text, :title => random_text, :description => random_text, :created_at => rand(2400).minutes.ago, :score => rand(50), :user => random_user}])
+  end
 end
 
-def create_submissions
-  50.times do
-    Submission.create([{:url => random_text, :title => random_text, :description => random_text, :created_at => rand(2400).minutes.ago, :score => rand(50), :user => User.find(rand(User.all.size) + 1)}])
+def create_comments
+  Submission.all.each do |submission|
+    rand(3).times do
+      comment = Comment.create([{:submission => submission, :text => random_text, :user => random_user}]).first
+      if rand(2) == 0
+        Comment.create([{:parent => comment, :submission => submission, :text => random_text, :user => random_user}])
+      end
+    end
   end
 end
 
 create_users
 create_submissions
-
-
+create_comments
 
