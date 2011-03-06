7002a7e02fccb05832646f31d2dfe6dfb64de4d9
Matt <matthieutc@yahoo.com>
2011-03-06 15:06:49 -0600

    added lots of seed data

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :username
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :karma
 
   has_many :submissions
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+@@text = %{The IMF's influence in the global economy steadily increased as it accumulated more members. The number of IMF member countries has more than quadrupled from the 44 states involved in its establishment, reflecting in particular the attainment of political independence by many developing countries and more recently the collapse of the Soviet bloc. The expansion of the IMF's membership, together with the changes in the world economy, have required the IMF to adapt in a variety of ways to continue serving its purposes effectively.}
+
+def create_users
+  users = ["john", "mike", "paul", "betty", "ashley"].map do |username|
+    john = {:username => username, :email => "#{username}@example.com", :password => '123456', :karma => rand(500)}
+  end
+
+  User.create(users)
+end
+
+def random_text
+  words = @@text.split
+  first = rand(words.size - 15)
+  last = rand(15)
+  words[first, last].join " "
+end
+
+def create_submissions
+  50.times do
+    Submission.create([{:url => random_text, :title => random_text, :description => random_text, :created_at => rand(2400).minutes.ago, :score => rand(50), :user => User.find(rand(User.all.size) + 1)}])
+  end
+end
+
+create_users
+create_submissions
+
+
+
