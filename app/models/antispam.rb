5af2b2ada02f0bba54c1bc33d7862de328b68ffb
Matt <matthieutc@yahoo.com>
2011-03-11 17:11:38 -0600

    fixed another bug when posting a child comment - fixed a bug in antispam.rb where we called text instead of description

--- app/models/antispam.rb
+++ app/models/antispam.rb
@@ -6,21 +6,26 @@ class Antispam
   end
 
   def is_spam? submission
-    @bayes.classify(submission.text) == :spam
+    @bayes.classify(wordify submission) == :spam
   end
 
   def switch_to_spam submission
-    @bayes.untrain(:content, "#{submission.title} #{submission.description}")
-    @bayes.train(:spam, "#{submission.title} #{submission.description}")
+    @bayes.untrain(:content, wordify(submission))
+    @bayes.train(:spam, wordify(submission))
     @madeleine.take_snapshot
   end
 
   def train_as_content submission
-    @bayes.train :content, "#{submission.title} #{submission.description}"
+    @bayes.train :content, wordify(submission)
     @madeleine.take_snapshot
   end
 
   def trained_entries
     @bayes.instance_variable_get :@total_words
+  end
+
+  private
+  def wordify submission
+    "#{submission.title} #{submission.description}"
   end
 end
