6ffd77d3383d1ba7c1661499b0475755b5e1a65f
Matt <matthieutc@yahoo.com>
2011-03-31 22:59:36 -0500

    upgraded to devise 1.2.1

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Socialite::Application.config.secret_token = '403aa4e4de38ef7d72160451e677d63a82cbd0813eabc6f85867de528fb3f1dfb2e241631ed5f37eb54afc26942dee7e1018e7a89805f24e108c72f9072c36dc'
+Socialite::Application.config.secret_token = '403aa4e4de38ef7d72160461e677d63a82cbd0813eabc6f85867de528fb3f1dfb2e241631ed5f37eb54afc26942dee7e1018e7a89805f24e108c72f9072c36dc'
--- vendor/gems/devise-1.1.7/lib/devise/version.rb
+++
@@ -1,3 +0,0 @@
-module Devise
-  VERSION = "1.1.7".freeze
-end
